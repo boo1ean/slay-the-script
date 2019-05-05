e3482090ac086462d8d57617c12e142b65d6f5c7
boo1ean <boo1ean0807@gmail.com>
2019-05-05 09:47:00 +0300

    Add resources

--- SlayTheSpireEnv.py
+++ SlayTheSpireEnv.py
@@ -1,8 +1,10 @@
 import json
-import random
 import logging
+
+import random
 from tabulate import tabulate
 from fabulous.color import bold, magenta_bg, green_bg, red_bg, red, green, magenta, blue
+
 from getch import getch
 from subprocess import call
 from time import sleep
@@ -27,7 +29,7 @@ def clear_screen():
 with open('cards.json', 'r') as cards_file:
     cards = json.loads(cards_file.read())
 
-decorations = [green, red]
+decorations = [None, green, red, red]
 def get_text_decoration(config):
     return decorations[config['id']]
 
@@ -57,6 +59,9 @@ class Creature():
         if amount > 0:
             self.hp -= amount
 
+    def is_player(self):
+        return False
+
 
 DEFAULT_PALYER_HP = 70
 DEFAULT_PLAYER_ENERGY = 3
@@ -65,7 +70,7 @@ DEFAULT_PLAYER_DRAW_RATE = 5
 
 class Player(Creature):
     # default deck is 6 defends + 6 strikes
-    default_deck = [Card(cards[0])] * 6 + [Card(cards[1])] * 6
+    default_deck = [Card(cards[0])] * 6 + [Card(cards[1])] * 6 + [Card(cards[2])] * 2
 
     # Initiall put whole deck to discard pile
     # and after initialization shuffle it to the draw pile
@@ -84,6 +89,16 @@ class Player(Creature):
         self.block = DEFAULT_PLAYER_BLOCK
 
         self.shuffle_discard_pile_to_draw_pile()
+
+    def get_hand_card(self, index):
+        try:
+            card = self.hand[index]
+            return card.config['id']
+        except IndexError:
+            return 0
+
+    def is_player(self):
+        return True
 
     # Shuffle cards in discard pile
     # and move it to draw pile
@@ -179,11 +194,16 @@ class Enemy(Creature):
         eff['damage'] += self.buff
         return eff
 
+    def get_damage_intent(self):
+        return self.action.effect()['damage']
+
     def render(self):
         hp_bar = bold('%s ❤️ %s/%s' % (bold(self.name), red(self.hp), red(self.max_hp)))
         intent = 'Going to deal %s %s' % (red(self.action.effect()['damage']), red('damage'))
 
         return '%s\n%s' % (hp_bar, intent)
+
+    damage_intent = property(get_damage_intent)
 
 class Battle():
     def __init__(self, player, enemy):
@@ -197,13 +217,16 @@ class Battle():
         self.player.draw(5)
 
     def step(self, action):
-        if action == 'e':
-            return self.end_turn()
         try:
             index = int(action)
-            effect = self.player.play_card(index - 1)
+
+            if index == 5:
+                return self.end_turn()
+
+            effect = self.player.play_card(index)
             if effect:
                 self.apply_effect(self.player, self.enemy, effect)
+
         except ValueError:
             return None
 
@@ -228,48 +251,133 @@ class Battle():
             return self.player
         return None
 
+import gym
+from gym import spaces
+
+
+N_DISCRETE_ACTIONS = 6
+N_DISCRETE_OBSERVATIONS=13
+
+# hp, max_hp, block, energy, max_energy, card1..card5, enemy_hp, enemy_max_hp, enemy_damange_intent
+class SlayTheScriptEnv(gym.Env):
+    def __init__(self):
+        super(SlayTheScriptEnv, self).__init__()
+        self.reset()
+        self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
+
+        self.observation_space = spaces.MultiDiscrete([
+            self.player.max_hp, #hp
+            self.player.max_hp, #max_hp
+            15, #block
+            3, #energy
+            3, #max_energy
+            len(cards), #card1
+            len(cards), #card2
+            len(cards), #card3
+            len(cards), #card4
+            len(cards), #card5
+            self.enemy.max_hp, #enemy_hp
+            self.enemy.max_hp, #enemy_max_hp
+            100, #enemy_max_hp
+        ])
+
+    def step(self, action):
+        assert self.action_space.contains(action)
+        reward = 0
+        info = {}
+
+        # if energy wasn't used - too bad
+        if action == 5 and self.player.energy > 0:
+            reward = -1
+
+        # if mana is used and no end turn - bad
+        if action != 5 and self.player.energy == 0:
+            reward = -1
+
+        # if try to play empty card slot - bad
+        if action != 5 and self.player.get_hand_card(action) == 0:
+            reward = -1
+
+        self.battle.step(action)
+        winner = self.battle.has_winner()
+
+        if winner and winner.is_player():
+            reward = 1 + 10 * winner.hp / winner.max_hp
+            info['winner'] = winner.name
+
+        return self._get_obs(), reward, bool(winner), info
+
+    def reset(self):
+        self.player = Player()
+        self.enemy = Enemy()
+        self.battle = Battle(player = self.player, enemy = self.enemy)
+        self.battle.start()
+        return self._get_obs()
+
+    def _get_obs(self):
+        return (
+            self.player.hp,
+            self.player.max_hp,
+            self.player.block,
+            self.player.energy,
+            self.player.max_energy,
+            self.player.get_hand_card(0),
+            self.player.get_hand_card(1),
+            self.player.get_hand_card(2),
+            self.player.get_hand_card(3),
+            self.player.get_hand_card(4),
+            self.enemy.hp,
+            self.enemy.max_hp,
+            self.enemy.damage_intent,
+        )
+
+
+    def render(self):
+        return self.battle.render()
+
+# env = SlayTheScriptEnv()
+# print(env.step(1))
 
 # class UserActor():
     # def __init__(self):
 
 
-p = Player()
-e = Enemy()
-b = Battle(player = p, enemy = e)
+# p = Player()
+# e = Enemy()
+# b = Battle(player = p, enemy = e)
 
-clear_screen()
-done = False
-b.start()
-while not done:
-    print(b.render())
-
-    print('''
-              ____      ________
-            ,^.__.>--"~~'_.--~_)~^.
-           _L^~   ~    (~ _.-~ \. |\\
-        ,-~    __    __,^"/\_A_/ /' \\
-      _/    ,-"  "~~" __) \  ~_,^   /\\
-     //    /  ,-~\ x~"  \._"-~     ~ _Y
-     Y'   Y. (__.//     /  " , "\_r ' ]
-     J-.__l_>---r{      ~    \__/ \_ _/
-    (_ (   (~  (  ~"---   _.-~ `\ / \ !
-     (_"~--^----^--------"  _.-c Y  /Y'
-      l~---v----.,______.--"  /  !_/ |
-       \.__!.____./~-.      _/  /  \ !
-        `x._\_____\__,>---"~___Y\__/Y'
-            ~     ~(_~~(_)"~___)/ /\|
-                   (_~~   ~~___)  \_t
-                   (_~~   ~~___)\_/ |
-                   (_~~   ~~___)\_/ |
-                   { ~~   ~~   }/ \ l
-    ''')
-    action = getch()
-    b.step(action)
-    clear_screen()
-    winner = b.has_winner()
-    if winner:
-        done = True
-        print('Winner is %s' % bold(winner.name))
+# clear_screen()
+# done = False
+# b.start()
+# while not done:
+    # print(b.render())
+    # print('''
+              # ____      ________
+            # ,^.__.>--"~~'_.--~_)~^.
+           # _L^~   ~    (~ _.-~ \. |\\
+        # ,-~    __    __,^"/\_A_/ /' \\
+      # _/    ,-"  "~~" __) \  ~_,^   /\\
+     # //    /  ,-~\ x~"  \._"-~     ~ _Y
+     # Y'   Y. (__.//     /  " , "\_r ' ]
+     # J-.__l_>---r{      ~    \__/ \_ _/
+    # (_ (   (~  (  ~"---   _.-~ `\ / \ !
+     # (_"~--^----^--------"  _.-c Y  /Y'
+      # l~---v----.,______.--"  /  !_/ |
+       # \.__!.____./~-.      _/  /  \ !
+        # `x._\_____\__,>---"~___Y\__/Y'
+            # ~     ~(_~~(_)"~___)/ /\|
+                   # (_~~   ~~___)  \_t
+                   # (_~~   ~~___)\_/ |
+                   # (_~~   ~~___)\_/ |
+                   # { ~~   ~~   }/ \ l
+    # ''')
+    # action = getch()
+    # b.step(action)
+    # clear_screen()
+    # winner = b.has_winner()
+    # if winner:
+        # done = True
+        # print('Winner is %s' % bold(winner.name))
 
 
 
